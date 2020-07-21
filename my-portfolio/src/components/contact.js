9c338c7d7b82f4ad7a1f5667682e2ced8c8eb6a7
Elena <elena.beccaro@gmail.com>
2020-07-21 17:13:10 +0100

    add nav bar in the scoll menu- contact dont woring

--- my-portfolio/src/components/contact.js
+++ my-portfolio/src/components/contact.js
@@ -4,7 +4,7 @@ class Contact extends Component {
     render() {
         return (
             <div>
-                <h1>Contact</h1>
+                <h1>contact</h1>
             </div>
         )
     }
