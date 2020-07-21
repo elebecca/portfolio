8b6b19f9eedc27c7c06b5901215cfd12c40b5767
Elena <elena.beccaro@gmail.com>
2020-07-21 16:19:43 +0100

    add recatmdl and set up the style

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
-
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="demo-big-content">
+      <Layout>
+        <Header title="Title" scroll>
+          <Navigation>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+          </Navigation>
+        </Header>
+        <Drawer title="Title">
+          <Navigation>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+          </Navigation>
+        </Drawer>
+        <Content>
+          <div className="page-content" />
+        </Content>
+      </Layout>
     </div>
   );
 }
