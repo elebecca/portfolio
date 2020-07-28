ed5e738af8f942af683ad7dc11b70f7c148bf69b
Elena <elena.beccaro@gmail.com>
2020-07-28 15:04:56 +0100

    add some style

--- my-portfolio/src/components/landingpage.js
+++ my-portfolio/src/components/landingpage.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 class Landing extends Component {
     render() {
         return (
-            <div>
-                <h1>Landing Page</h1>
+            <div style={{ with: '100%', margin: 'auto' }}>
+                <Grid className='landing-grid'>
+                    <Cell col={12}>
+                        <img
+                            src='https://freesvg.org/img/Female-Avatar-5.png'
+                            alt='avatar'
+                            className='avatar-img'
+                        />
+                        <div className='banner-text'>
+                            <h1>Frontend Developer</h1>
+                        </div>
+                    </Cell>
+                </Grid>
             </div>
         )
     }
