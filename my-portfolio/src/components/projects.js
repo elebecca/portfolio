9bb4af3285d34f1e6b868350da5aa74fc9011b43
Elena <elena.beccaro@gmail.com>
2020-07-30 14:53:44 +0100

    create tabs for project

--- my-portfolio/src/components/projects.js
+++ my-portfolio/src/components/projects.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import { Tabs, Tab } from 'react-mdl';
 
 class Projects extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { activeTab: 0 }
+    }
     render() {
         return (
             <div>
-                <h1>Projects</h1>
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
+                    <Tab>Ruby</Tab>
+                    <Tab>Rails</Tab>
+                    <Tab>Javascipt</Tab>
+                    <Tab>React</Tab>
+                </Tabs>
             </div>
         )
     }
