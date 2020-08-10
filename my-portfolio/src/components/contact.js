836942daf3a565c118c16afc4b844c5ac0bea15b
Elena <elena.beccaro@gmail.com>
2020-08-10 16:59:41 +0100

    add list

--- my-portfolio/src/components/contact.js
+++ my-portfolio/src/components/contact.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Cell } from 'react-mdl';
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
 
 class Contact extends Component {
     render() {
@@ -12,10 +12,31 @@ class Contact extends Component {
                             alt='avatar'
                             className='avatar-img' />
                         <p style={{ width: '75%', margin: 'auto', paddingTop: '1em' }}>
-                            I am a passionate developer with a strong background in graphic design. I'm fascinated about the intersection of technology of creativity. I’ve good communication skills, a can-do attitude and am always looking to go the extra mile. Recently I’ve been a graduate in Makers Academy (London's leading programming bootcamp) in order to become upskilled with best practice tools and methodologies.
+                            I am a passionate developer with a strong background in graphic design.
+                            I'm fascinated about the intersection of technology of creativity.
+                            I’ve good communication skills, a can-do attitude and am always looking to go the extra mile.
+                            Recently I’ve been a graduate in Makers Academy (London's leading programming bootcamp) in order
+                            to become upskilled with best practice tools and methodologies.
                         </p>
                     </Cell>
-                    <Cell col={6}>half page</Cell>
+                    <Cell col={6}>
+                        <h2>Contact Me</h2>
+                        <hr />
+
+                        <div className='contact-list'>
+                            <List>
+                                <ListItem>
+                                    <ListItemContent icon="person">Bryan Cranston</ListItemContent>
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemContent icon="person">Aaron Paul</ListItemContent>
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemContent icon="person">Bob Odenkirk</ListItemContent>
+                                </ListItem>
+                            </List>
+                        </div>
+                    </Cell>
                 </Grid>
 
             </div>
