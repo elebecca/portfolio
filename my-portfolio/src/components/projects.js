13716b176bd7e9cc6c9b17721b7940ad98943fa0
Elena <elena.beccaro@gmail.com>
2020-08-10 11:04:31 +0100

    Add some more projects

--- my-portfolio/src/components/projects.js
+++ my-portfolio/src/components/projects.js
@@ -15,7 +15,7 @@ class Projects extends Component {
                 <div className='projects-grid'>
                     {/* Project 1 */}
                     <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
-                        <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://github.com/elebecca/Minefield/blob/master/screenshot.png?raw=true) center / cover' }} >Minefield</CardTitle>
+                        <CardTitle style={{ color: '#000000', height: '176px', background: 'url(https://github.com/elebecca/Minefield/blob/master/screenshot.png?raw=true) center / cover' }} >Minefield</CardTitle>
                         <CardText>
                             Build a game web application that allow you to play to minefield. The goal of the game is to find the beer follow the arrow.Be careful, if you find the skull you will lose. This is my first attempt to create somthing with Javascript.
                             The web site is fully responsive
@@ -30,7 +30,7 @@ class Projects extends Component {
 
                     {/* Project 2 */}
                     <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
-                        <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://github.com/elebecca/10_local_storage/blob/master/screenshot.PNG?raw=true) center / cover' }} >Locoal Storage</CardTitle>
+                        <CardTitle style={{ color: '#000000', height: '176px', background: 'url(https://github.com/elebecca/10_local_storage/blob/master/screenshot.PNG?raw=true) center / cover' }} >Locoal Storage</CardTitle>
                         <CardText>
                             Learning how to use local storage and converting data in JSON
             </CardText>
@@ -45,7 +45,7 @@ class Projects extends Component {
 
                     {/* Project 3 */}
                     <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
-                        <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://github.com/elebecca/02_JS_Clock/raw/master/screenshot.PNG) center / cover' }} >Clock</CardTitle>
+                        <CardTitle style={{ color: '#000000', height: '176px', background: 'url(https://github.com/elebecca/02_JS_Clock/raw/master/screenshot.PNG) center / cover' }} >Clock</CardTitle>
                         <CardText>
                             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
             </CardText>
@@ -60,7 +60,7 @@ class Projects extends Component {
 
                     {/* Project 4 */}
                     <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
-                        <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://github.com/elebecca/05_Flex_panel_gallery/raw/master/screenshot_1.PNG) center / cover' }} >Galley</CardTitle>
+                        <CardTitle style={{ color: '#000000', height: '176px', background: 'url(https://github.com/elebecca/05_Flex_panel_gallery/raw/master/screenshot_1.PNG) center / cover' }} >Galley</CardTitle>
                         <CardText>
                             build a Galley with Javascript and flexbox.
             </CardText>
@@ -79,7 +79,7 @@ class Projects extends Component {
         } else if (this.state.activeTab === 1) {
             return (
                 <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
-                    <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://github.com/elebecca/COVID-19-Tracker-Application/raw/master/screenshot.PNG) center / cover' }}>Covid Tracker</CardTitle>
+                    <CardTitle style={{ color: '#000000', height: '176px', background: 'url(https://github.com/elebecca/COVID-19-Tracker-Application/raw/master/screenshot.PNG) center / cover' }}>Covid Tracker</CardTitle>
                     <CardText>
                         Create a full COVID-19 Tracker
                         </CardText>
@@ -94,14 +94,23 @@ class Projects extends Component {
             )
         } else if (this.state.activeTab === 2) {
             return (
-                <div><h1>This is VueJS</h1></div>
-            )
-        } else if (this.state.activeTab === 3) {
-            return (
-                <div><h1>This is MongoDB</h1></div>
+                <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+                    <CardTitle style={{ color: '#000000', height: '176px', background: 'url(https://github.com/elebecca/CV/raw/master/postbox.PNG) center / cover' }}>Covid Tracker</CardTitle>
+                    <CardText>
+                        PostBox is a Virtual gifting app.In these times of isolation it’s more important than ever to spread happiness. This is a virtual care packages to send to family, friends and other loved ones.
+                        </CardText>
+                    <CardActions border>
+                        <Button colored href='https://github.com/elebecca/MakersPostBox'>GitHub</Button>
+                        <Button colored href='https://makers-postbox.herokuapp.com/'>Live</Button>
+                    </CardActions>
+                    <CardMenu style={{ color: '#fff' }}>
+                        <IconButton name="share" />
+                    </CardMenu>
+                </Card>
+
+
             )
         }
-
     }
 
 
@@ -113,7 +122,6 @@ class Projects extends Component {
                     <Tab>Javascript</Tab>
                     <Tab>React</Tab>
                     <Tab>Ruby</Tab>
-                    <Tab>MongoDB</Tab>
                 </Tabs>
 
 
