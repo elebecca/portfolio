a49154146003fb0a4c0bfd8b7e45c043ec728f39
Elena <elena.beccaro@gmail.com>
2020-08-11 14:39:14 +0100

    Start the resume page

--- my-portfolio/src/components/resume.js
+++ my-portfolio/src/components/resume.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 class Resume extends Component {
     render() {
         return (
             <div>
-                <h1>Resume</h1>
+                <Grid>
+                    <Cell col={4}>Left side</Cell>
+                    <Cell className='resume-right-col' col={8}>Right side</Cell>
+                </Grid>
             </div>
         )
     }
